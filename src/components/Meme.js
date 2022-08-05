17ac706178f35c35fbb4f00e3a1cfc1d1fe33ffc
Delta0077 <adas0262@gmail.com>
2022-08-06 01:00:39 +0530

    Added top and bottom text field with controlled state

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,6 +1,5 @@
 import React from "react";
 import memesData from "../memesData.js";
-import "../Styles/Meme.css";
 
 export default function Meme() {
   // const [memeImage, setMemeImage] = React.useState("http://i.imgflip.com/1bij.jpg");
@@ -24,16 +23,42 @@ export default function Meme() {
     }));
   }
 
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+
   return (
     <main>
       <div className="form">
-        <input type="text" placeholder="Top text" className="form--input" />
-        <input type="text" placeholder="Bottom text" className="form--input" />
+        <input
+          type="text"
+          placeholder="Top text"
+          className="form--input"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Bottom text"
+          className="form--input"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
         <button className="form--button" onClick={getMemeImage}>
           Get a new meme image 🖼️
         </button>
       </div>
-      <img src={meme.randomImage} alt="" className="image--container" />
+      <div className="meme">
+        <img src={meme.randomImage} className="meme--image" />
+        <h2 className="meme--text top">{meme.topText}</h2>
+        <h2 className="meme--text bottom">{meme.bottomText}</h2>
+      </div>
     </main>
   );
 }
